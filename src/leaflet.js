a26c6b2eb6a391f0cfdaed0133052fe3db2606ba
LimitToInfinity <21stLimitless@gmail.com>
2020-06-12 00:29:17 -0600

    Adds map of Denver.

--- src/leaflet.js
+++ src/leaflet.js
@@ -1,1 +1,13 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+import { config } from "../config.js";
+const { accessToken } = config;
+
+const map = L.map('map').setView([39.741461153984844, -104.99749943953447], 13);
+
+L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`, {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken
+}).addTo(map);
